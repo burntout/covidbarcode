49e582d0df8bcea944ace64b5e077e7ef40b2913
Kate Dawson <kate@webarchitects.coop>
2021-08-31 01:38:31 +0100

    cache signing keys

--- verify.py
+++ verify.py
@@ -13,8 +13,13 @@ def read_qr(image="myCovidPass-2.png"):
 PROD_COVID_KEYS = "https://covid-status.service.nhsx.nhs.uk/pubkeys/keys.json" 
 
 def getCovidSigningKeys(covidKeysURL = PROD_COVID_KEYS):
-    with urllib.request.urlopen(covidKeysURL) as url:
-        return json.loads(url.read().decode())
+    try:
+        with urllib.request.urlopen(covidKeysURL) as url:
+            pubKeys = json.loads(url.read().decode())
+    except: 
+        with open('cache/keys.json') as f:
+            pubKeys = json.load(f)
+    return pubKeys
 
 def parse_payload(p):
     payload = base64.b64decode(add_b64_padding(p))
@@ -24,6 +29,7 @@ def parse_payload(p):
 
 header, payload, signature = read_qr().split(".")
 pubKeys = getCovidSigningKeys()
+
 ecPubKey = [i['publicKey'] for i in pubKeys if i['kid'] == add_b64_padding(header)][0]
 byteData = (header + "." + payload).encode("utf-16le")
 sigBytes = base64.b64decode(add_b64_padding(signature), "-_")
